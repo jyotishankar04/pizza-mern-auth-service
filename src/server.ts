2fc97928aab2ab574b133ed7fbf14dfa2b37e212
pipalii <bignyajyotipatra@gmail.com>
2025-07-30 23:06:01 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ function welcomeMessage(user: { name: string; email: string }) {
     const anotherUser = {
         name: "jyotishankar",
         email: "jyotishankar@qwikish.com",
-    }
+    };
     return `Welcome ${user.name} your email is ${user.email}`;
 }
 
