17215e9e94b90a99ac59b0d92579db4ed78bd560
jyotishankar04 <patrajyotishankar@gmail.com>
2025-08-04 16:48:18 +0530

    add tanents

--- src/migration/1754304911880-create_tenants_table.ts
+++ src/migration/1754304911880-create_tenants_table.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateTenantsTable1754304911880 implements MigrationInterface {
-    name = 'CreateTenantsTable1754304911880'
+    name = "CreateTenantsTable1754304911880";
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "tanents" ("id" SERIAL NOT NULL, "name" character varying(100) NOT NULL, "address" character varying(255) NOT NULL, "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_30385283cb7e4b040efdb0ddcec" PRIMARY KEY ("id"))`);
+        await queryRunner.query(
+            `CREATE TABLE "tanents" ("id" SERIAL NOT NULL, "name" character varying(100) NOT NULL, "address" character varying(255) NOT NULL, "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_30385283cb7e4b040efdb0ddcec" PRIMARY KEY ("id"))`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "tanents"`);
     }
-
 }
