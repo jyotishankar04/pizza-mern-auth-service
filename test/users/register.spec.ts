d94873eddbea557d1eb9f37f7ce64fe29c6c3500
pipalii <bignyajyotipatra@gmail.com>
2025-08-07 16:09:00 +0530

    replace bcrypt with bcrypt js

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -135,7 +135,7 @@ describe("POST /auth/register", () => {
             expect(users[0]).toHaveProperty("password");
             expect(users[0].password).not.toBe(payload.password);
             expect(users[0].password).toHaveLength(60);
-            expect(users[0].password).toMatch(/^\$2b\$\d+\$/);
+            expect(users[0].password).toMatch(/^\$2[b|a]\$\d+\$/);
         });
         it("should store unique, lowercase and valid email", async () => {
             // AAA
