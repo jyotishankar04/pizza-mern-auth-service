4201887fa1cd823702d24de7449789f0c433d6b4
jyotishankar04 <patrajyotishankar@gmail.com>
2025-09-26 16:50:42 +0530

    fixing migration

--- src/migration/1758734154940-init.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class Init1758734154940 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {}
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
